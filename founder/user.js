73844b11e2dafdcd15fcfa74e78a3a30823b0ad7
tabsharecloud <41350572+tabsharecloud@users.noreply.github.com>
2018-07-21 10:34:52 -0700

    Add files via upload

---
+++ founder/user.js
@@ -0,0 +1,14 @@
+dir="https://f002.backblazeb2.com/file/tabshare/founder/"
+
+loadScript(dir+"2018imeta.tabshare.js");
+loadScript(dir+"2017imeta.tabshare.js");
+
+function loadRecord(){
+  a=location.href; hash=a.slice(a.indexOf('#'));
+  wait();
+  rec=location.hash.replace('#tabshare','')
+  loadScript(dir+'tabshares/compiled/'+rec+'.tabshare.js');
+};
+
+if(location.href.indexOf('#tabshare')){loadRecord()}
+  
