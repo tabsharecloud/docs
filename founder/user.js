3fecbf9d04550306a6138dbf81199125d2cee619
tabsharecloud <41350572+tabsharecloud@users.noreply.github.com>
2018-07-27 13:31:10 -0700

    Update user.js

--- founder/user.js
+++ founder/user.js
@@ -1,4 +1,5 @@
-dir="https://f002.backblazeb2.com/file/tabshare/founder/"
+//dir="https://f002.backblazeb2.com/file/tabshare/founder/"
+dir="https://user-a6eb349d.cdn.memsites.com/";
 
 loadScript(dir+"2018imeta.tabshare.js");
 loadScript(dir+"2017imeta.tabshare.js");
