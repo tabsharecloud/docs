dde43908f8916f97dda8620fa87f00be58f3ce3d
tabsharecloud <41350572+tabsharecloud@users.noreply.github.com>
2018-08-05 19:36:00 -0700

    Update user.js

--- founder/user.js
+++ founder/user.js
@@ -3,13 +3,14 @@ var store="https://nimble-fig.cloudvent.net/users/founder";
 var liveDir=store+"/live/";
 var staticDir=store+"/static/";
 
-loadScript(liveDir+"2018imeta.tabshare.js");
+loadScript(liveDir+"2018imeta.tabshare.js?v=0");
 loadScript(liveDir+"2017imeta.tabshare.js");
 
 function loadRecord(){
   a=location.href; hash=a.slice(a.indexOf('#'));
-  wait(); rec=location.hash.replace('#tabshare','')
+  wait(); rec=location.hash.replace('#tabshare','');
   loadScript(staticDir+'tabjs/'+rec+'.tabshare.js');
 };
 
-if(location.href.indexOf('#tabshare')){loadRecord()}
+document.body.style.backgroundImage="url('https://cdn.tabshare.cloud/img/real-carbon-fibre.png')";
+if(location.href.indexOf('#tabshare')>0){loadRecord()}
