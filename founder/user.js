908edce8b4fb389a6d8b6d364bf658126dd38205
tabsharecloud <41350572+tabsharecloud@users.noreply.github.com>
2018-08-29 14:16:37 -0700

    Update user.js

--- founder/user.js
+++ founder/user.js
@@ -14,10 +14,13 @@ function loadRecord(){a=location.href;
 if(location.href.indexOf('#tabshare')>0){loadRecord()}
 
 // lazy load icos and backgound img from cdn
-var cicdn='https://a1ugk98en.cloudimg.io/cdno/n/n/https://img.tabshare.cloud';
+//var cicdn='https://a1ugk98en.cloudimg.io/cdno/n/n/https://img.tabshare.cloud';
+var cicdn='https://img.tabshare.cloud';
 document.body.style.backgroundImage='url('+cicdn+'/paper/escher-sq.png)';
+
 $id('png-ico').href=cicdn+'/ico/favicon-16x16.png';
 $id('touch-ico').href=cicdn+'/ico/apple-touch-icon.png';
 $id('mac-ico').href=cicdn+'/ico/safari-pinned-tab.svg';
+<!--[if IE]>$id('old-ico').href=cicdn+'/ico/favicon.ico';<![endif]-->
 
-// $id('old-ico').href=cicdn+'/ico/favicon.ico';
+
